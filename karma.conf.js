7f33d3b970a33b139f5982cc422cab7857383d77
hugodesb <hugo.desbiolles@gmail.com>
2021-01-06 10:16:31 +0100

    change name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/crossds-client'),
+      dir: require('path').join(__dirname, './coverage/matelist'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'crossds-client';
+  title = 'matelist';
   displayError = false;
-  
+
 }
